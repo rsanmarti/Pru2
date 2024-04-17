9153b149899abd109b094175d80ced7a64527c41
rsanmarti <82457692+rsanmarti@users.noreply.github.com>
2024-04-17 10:23:29 +0200

    Update Hola.java

--- PruebaXXX/src/Hola.java
+++ PruebaXXX/src/Hola.java
@@ -2,9 +2,9 @@
 public class Hola {
 
 	public Hola() {
-		// TODO Esbozo de constructor generado autom·ticamente
+		// TODO Esbozo de constructor generado autom√°ticamente
 		
-		System.out.println();
+		System.out.println("          ");
 	}
 
 }
